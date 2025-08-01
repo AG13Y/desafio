9f7a82edcb961387923ede00b030d816cc902fce
AG13Y <henrique.augusto.de.lima@gmail.com@gmail.com>
2025-08-01 17:42:40 -0300

    crud-feito

--- src/app/pages/dictionary-registration/dictionary-word/dictionary-word.ts
+++ src/app/pages/dictionary-registration/dictionary-word/dictionary-word.ts
@@ -2,6 +2,8 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DicionaryService } from '../../../shared/services/dicionary.services';
 import { WordService } from '../../../shared/services/word.services';
+import { ModalWord } from '../modal-word/modal-word';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-dictionary-word',
@@ -10,25 +12,33 @@ import { WordService } from '../../../shared/services/word.services';
   styleUrl: './dictionary-word.css'
 })
 export class DictionaryWord {
+  bsModalRef?: BsModalRef;
+  
   dictionary: any;
   palavras: any[] = [];
   route = inject(ActivatedRoute);
   dicionaryService = inject(DicionaryService);
   wordService = inject(WordService);
+  
+  constructor(private modalService: BsModalService) {}
 
   ngOnInit() {
-    const id = String(this.route.snapshot.paramMap.get('id'));
-    if (id) {
-      this.dicionaryService.getDictionary(id).subscribe(data => {
-        this.dictionary = data;
-
-        this.wordService.getDictionaryTexts().subscribe(textos => {
-          
-          this.palavras = textos.filter(texto => texto.dicionarioId === id);
-        });
-      });
-    }  
+  const id = String(this.route.snapshot.paramMap.get('id'));
+  if (id) {
+    this.dicionaryService.getDictionary(id).subscribe(data => {
+      this.dictionary = data;
+      this.getPalavras();
+    });
   }
+}
+  
+  getPalavras() {
+  const id = String(this.route.snapshot.paramMap.get('id'));
+  this.wordService.getDictionaryTexts().subscribe(textos => {
+    this.palavras = textos.filter(texto => texto.dicionarioId === id);
+  });
+}
+
   deletePalavra(id: string) {
     const confirmDelete = window.confirm("Deseja excluir esta palavra?");
     if (confirmDelete) {
@@ -38,5 +48,15 @@ export class DictionaryWord {
       });
     }
   }
+
+  openModal(texto?: any) {
+  const payload = texto || { dicionarioId: this.dictionary.id };
+  this.bsModalRef = this.modalService.show(ModalWord, {
+    initialState: {
+      texto: payload,
+      onSave: () => this.getPalavras()
+    }
+  });
+}
 
 }
--- src/app/pages/dictionary-registration/modal-word/modal-word.ts
+++ src/app/pages/dictionary-registration/modal-word/modal-word.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { WordService } from '../../../shared/services/word.services';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-modal-word',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './modal-word.html',
   styleUrl: './modal-word.css'
 })
 export class ModalWord {
+  texto: any;
+  form!: FormGroup;
+  onSave?: () => void;
 
+  wordService = inject(WordService);
+  fb = inject(FormBuilder);
+
+  constructor(public bsModalRef: BsModalRef) { }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      texto: [this.texto?.texto || '', Validators.required],
+      definicao: [this.texto?.definicao || ''],
+      definicao_extra: [this.texto?.definicao_extra || ''],
+      dicionarioId: [this.texto?.dicionarioId || '', Validators.required]
+    });
+  }
+
+  closeModal() {
+    this.bsModalRef.hide();
+  }
+
+  saveEdit() {
+    if (this.form.invalid) return;
+
+    if (this.texto?.id) {
+      // Editar texto existente
+      this.wordService.putDictionaryTexts(this.texto.id, this.form.value).subscribe(() => {
+        if (this.onSave) this.onSave();
+        this.closeModal();
+      });
+    } else {
+      // Criar novo texto
+      this.wordService.postDictionaryTexts(this.form.value).subscribe(() => {
+        if (this.onSave) this.onSave();
+        this.closeModal();
+      });
+    }
+  }
 }
