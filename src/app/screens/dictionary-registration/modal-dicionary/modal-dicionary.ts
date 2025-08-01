10fa2383277d83ac12303b5342b222ccfcbaccd0
AG13Y <henrique.augusto.de.lima@gmail.com@gmail.com>
2025-08-01 10:41:58 -0300

    modal-save

--- src/app/screens/dictionary-registration/modal-dicionary/modal-dicionary.ts
+++ src/app/screens/dictionary-registration/modal-dicionary/modal-dicionary.ts
@@ -1,6 +1,7 @@
 import { Component, inject, signal } from '@angular/core';
 import { BsModalRef} from 'ngx-bootstrap/modal';
 import { FormsModule } from '@angular/forms';
+import { DicionaryService } from '../../../shared/services/dicionary.services';
 
 
 @Component({
@@ -11,12 +12,25 @@ import { FormsModule } from '@angular/forms';
 })
 export class ModalDicionary {
   dictionary: any;
-  
+  editDictionary: any = {};
+
+  dictionaryService = inject(DicionaryService);
+
   constructor(public bsModalRef: BsModalRef) {}
+
+  ngOnInit() {
+    this.editDictionary = { ...this.dictionary };
+  }
 
   closeModal() {
     this.bsModalRef.hide();
   }
 
+  saveEdit() {
+    this.dictionaryService.putDictionary(this.editDictionary.id, this.editDictionary).subscribe(() => {
+      this.dictionary.nome = this.editDictionary.nome;
+      this.closeModal();
+    });
+  }
 
 }
